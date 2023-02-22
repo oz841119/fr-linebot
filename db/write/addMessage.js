1029a15bb1a086731d6d437632c167252b7cce01
YiliChen <yili@YiliChendeMacBook-Air.local>
2023-02-23 01:43:26 +0800

    fix: timezone

--- db/write/addMessage.js
+++ db/write/addMessage.js
@@ -1,10 +1,12 @@
 const dbClient = require('../db.js')
 const dayjs = require('dayjs')
+const timezone = require('dayjs/plugin/timezone')
+dayjs.extend(timezone)
 
 async function writeDb(lineUserId, message) {
   await dbClient.connect()
   const lineMesssageColl = await dbClient.db("line_bot").collection('line_user_message');
-  await lineMesssageColl.insertOne({date: dayjs().format('YYYY-MM-DD'), line_user_id: lineUserId, message: message})
+  await lineMesssageColl.insertOne({date: dayjs().tz('Asia/Taipei').format('YYYY-MM-DD'), line_user_id: lineUserId, message: message})
   dbClient.close()
   Promise.resolve()
 }
