a71b210ba0c3004815f4c8bbb8642efe894c1a91
oz841119 <oz841119@gmail.com>
2023-02-24 01:29:44 +0800

    test: routes

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const line_bot = require('./routes/line_bot')
+const get_message = require('./routes/get_message')
 app.use('/line_bot', line_bot)
+app.use('/get_message', get_message)
 
 app.listen(port, () => {
   console.log(`listening on ${port}`);
