a833f7796b1b476ea6f1a6a5adfbf58d18f4bc35
oz841119 <oz841119@gmail.com>
2023-02-25 16:58:28 +0800

    fix: cors

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors')
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3333;
 
 
 const line_bot = require('./routes/line_bot')
 const get_message = require('./routes/get_message')
+app.use(cors());
 app.use('/line_bot', line_bot)
 app.use('/get_message', get_message)
-app.use(cors());
 
 app.listen(port, () => {
   console.log(`listening on ${port}`);
