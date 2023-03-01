de89dd51d3a5d8f7974ef124f42c54ad1f5e7d5e
oz841119 <oz841119@gmail.com>
2023-03-02 00:46:06 +0800

    fix: line bot

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const line_bot = require('./routes/line_bot')
 const get_message = require('./routes/get_message')
 const add_action_record = require('./routes/add_action_record')
 app.use(cors());
-app.use(express.json());
 app.use('/line_bot', line_bot)
+app.use(express.json());
 app.use('/get_message', get_message)
 app.use('/add_action_record', add_action_record)
 
