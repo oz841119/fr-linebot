1b3197cda95123db81cd62e4d174083ea60b349f
chen yili <oz841119@gmail.com>
2023-03-11 17:32:01 +0800

    fix: Line bot write

--- controllers/addMessage.js
+++ controllers/addMessage.js
@@ -1,1 +1,16 @@
+const dbClient = require('../db.js')
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/timezone')
+const timezone = require('dayjs/plugin/utc')
+dayjs.extend(timezone)
+dayjs.extend(utc)
 
+const db = dbClient.db("fitness_record_db")
+
+async function writeDb(lineUserId, message) {
+  await dbClient.connect()
+  const lineMesssageColl = await db.collection('line_message');
+  await lineMesssageColl.insertOne({time: new Date(), line_user_id: lineUserId, message: message})
+  Promise.resolve()
+}
+module.exports = writeDb
